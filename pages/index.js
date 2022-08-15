5c8fe748b935398ee40a72dda40c40e6e86b2339
Laderlappen <elmervermeulen@gmail.com>
2022-08-15 12:10:58 +0200

    Changed styles

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,21 @@ export default function Home() {
             <Head>
                 <title>DB Capital</title>
                 <meta name="description" content="DB Capital Website" />
-                <link rel="icon" href="/dart-arrow.svg" />
+                <link rel="icon" href="/DB_Logo_only_image.ico" />
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Dream Big Capital</h1>
-
+                <div className={styles.titleRow}>
+                    <h1 className={styles.title}>Dream Big Capital</h1>
+                    <div className={styles.logoImg}>
+                        <Image
+                            width={100}
+                            height={100}
+                            src="/DB_Logo_only_image_no_back.png"
+                            className={"image"}
+                        />
+                    </div>
+                </div>
                 <p className={styles.description}>
                     Get started by downloading our trading software:{" "}
                     <code className={styles.code}>
